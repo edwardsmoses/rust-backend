cb2dfc8740437859109c0cd2452d819ab7d7d147
Edwards Moses <edwardsmoses3@gmail.com>
2022-08-10 20:51:57 +0100

    write tests for the hello route

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-#[macro_use] use rocket::*;
-use rocket_contrib::serve::StaticFiles;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+use rocket::*;
 use rocket_contrib::helmet::SpaceHelmet;
+use rocket_contrib::serve::StaticFiles;
 
 #[get("/")]
 fn index() -> String {
@@ -16,9 +18,30 @@ fn hello(name: String, age: u8) -> String {
 }
 
 fn main() {
+    rocket().launch();
+}
+
+fn rocket() -> rocket::Rocket {
     rocket::ignite()
-    .attach(SpaceHelmet::default())
-    .mount("/", routes![hello, index])
-    .mount("/files", StaticFiles::from("static/"))
-    .launch();
+        .attach(SpaceHelmet::default())
+        .mount("/", routes![hello, index])
+        .mount("/files", StaticFiles::from("static/"))
+}
+
+
+
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn hello_test() {
+        let client = Client::new(rocket()).expect("Valid rocket instance");
+        let mut response = client.get("/hello/edwards/23").dispatch();
+        assert_eq!(response.status(), Status::Ok); 
+        assert_eq!(response.body_string(), Some("Hello, 23 year old named edwards!".into()));
+    }
 }
