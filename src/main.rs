bfb895bf7e5ae73532ab4cd34351dafdc04b40e9
Edwards Moses <edwardsmoses3@gmail.com>
2022-08-10 20:40:12 +0100

    file server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] use rocket::*;
+use rocket::response::NamedFile;
+
+use std::path::{Path, PathBuf};
 
 #[get("/")]
 fn index() -> String {
@@ -12,6 +16,12 @@ fn hello(name: String, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
+#[get("/file/<file..>")]
+fn fileserver(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![hello, index]).launch();
+    rocket::ignite().mount("/", routes![hello, index, fileserver]).launch();
 }
